41e22712f40b9c9752e34a2acffbbda1624a0007
t_ruth_less <s1645316@ed.ac.uk>
2019-09-26 19:57:58 +0100

    nae bother pal

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import pygame
 screen = pygame.display.set_mode((width, height))
 background_colour = (255,182,193)
 screen.fill(background_colour)
-pygame.display.flip()
 
+pygame.draw.polygon(screen, (244,233,222) , [[100, 100], [0, 200], [200, 200]], 5)
+
+pygame.display.flip()
 
 running = True
 while running:
